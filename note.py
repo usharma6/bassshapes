e52d73b9e54381bff30cc243bb63a10f50e1e04d
Udit Sharma <usharma6@jhu.edu>
2020-04-21 15:33:01 -0400

    made note class

---
+++ note.py
@@ -0,0 +1,8 @@
+
+class Note:
+    def __init__(self, length, pitch)
+        self.length = length
+        self.pitch = pitch
+
+    def __str__(self):
+        return "length: " + self.length + "\n" + "pitch: " + self.pitch + "\n"
